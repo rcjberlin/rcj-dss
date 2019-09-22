83fab545230e76ff8cc4a32305001e86c155bc95
Niko Lockenvitz <n.lockenvitz@online.de>
2019-09-22 19:29:39 +0200

    load arenas/rounds/teams from json + save data to local storage (currently only for first screen S1)

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const LS_CURRENT_SCREEN = "currentScreen";
+const LS_DATA           = "data";
 
 const GAP          = "gap";
 const OBSTACLE     = "obstacle";
 const SPEEDBUMP    = "speedbump";
 const RAMP         = "ramp";
 const INTERSECTION = "intersection";
+
+let data = {};
 
 let timeOffset = 0.0;
 let timeStartedTimestamp = null;
@@ -14,16 +17,21 @@ let url = new URL(window.location.href);
 
 let getTime = function () {
 	return (new Date).getTime() / 1000;
-}
+};
 
 window.onload = function() {
 	adjustSizeOfMainUI();
 	
 	addEventListenersForNavigationButtons();
 	addEventListenersForButtons();
+	addEventListenersForInputs();
 	addEventListenersForScoringElementButtons();
 	
+	loadDataFromLocalStorage();
+	initializeMissingData();
+	
 	showInitialScreen();
+	initializeInputs();
 };
 
 let isTimeRunning = function () {
@@ -104,6 +112,80 @@ let addEventListenersForButtons = function () {
 	});
 };
 
+let addEventListenersForInputs = function () {
+	document.getElementById("referee-name").addEventListener("change", onChangeInputRefereeName);
+	document.getElementById("referee-password").addEventListener("change", onChangeInputRefereePassword);
+	document.getElementById("competition").addEventListener("change", onChangeInputCompetition);
+	document.getElementById("arena").addEventListener("change", onChangeInputArena);
+	document.getElementById("round").addEventListener("change", onChangeInputRound);
+};
+
+let onChangeInputRefereeName = function () {
+	data["referee"]["name"] = document.getElementById("referee-name").value;
+	saveDataToLocalStorage();
+};
+
+let onChangeInputRefereePassword = function () {
+	data["referee"]["password"] = document.getElementById("referee-password").value;
+	saveDataToLocalStorage();
+};
+
+let onChangeInputCompetition = function () {
+	let selectedCompetition = document.getElementById("competition").value;
+	if (selectedCompetition !== "line" && selectedCompetition !== "entry") {
+		selectedCompetition = "line";
+		document.getElementById("competition").value = selectedCompetition;
+	}
+	
+	let competitionInfo = JSON.parse(competitionJSON)[selectedCompetition];
+	
+	setSelectInputOptions("arena", competitionInfo["arenas"]);
+	setSelectInputOptions("round", competitionInfo["rounds"]);
+	setSelectInputOptions("teamname", competitionInfo["teams"]);
+	
+	// evacuation point
+	document.getElementById("evacuation-point-low").checked = true;
+	if (selectedCompetition === "entry") {
+		document.getElementById("evacuation-point-low").disabled = true;
+		document.getElementById("evacuation-point-high").disabled = true;
+	} else {
+		document.getElementById("evacuation-point-low").disabled = false;
+		document.getElementById("evacuation-point-high").disabled = false;
+	}
+	
+	// save to data / Local Storage
+	data["competition"] = selectedCompetition;
+	data["arena"] = document.getElementById("arena").value;
+	data["round"] = document.getElementById("round").value;
+	
+	saveDataToLocalStorage();
+};
+
+let onChangeInputArena = function () {
+	data["arena"] = document.getElementById("arena").value;
+	saveDataToLocalStorage();
+};
+
+let onChangeInputRound = function () {
+	data["round"] = document.getElementById("round").value;
+	saveDataToLocalStorage();
+};
+
+let setSelectInputOptions = function (selectId, options) {
+	let selectInput = document.getElementById(selectId);
+	
+	// remove all options
+	selectInput.options.length = 0;
+	
+	// add option for all elements in passed array
+	options.sort();
+	for (let i=0; i < options.length; i++) {
+		selectInput.options[selectInput.options.length] = new Option(options[i], options[i], false, false);
+	}
+	
+	selectInput.value = "";
+};
+
 let addEventListenersForScoringElementButtons = function () {
 	let arr = [ {imgId: "img-gap",          name: GAP},
 				{imgId: "img-obstacle",     name: OBSTACLE},
@@ -135,6 +217,35 @@ let adjustSizeOfMainUI = function () {
 	}
 };
 
+let loadDataFromLocalStorage = function () {
+	data = localStorage.getItem(LS_DATA);
+	if (data === null) {
+		data = {};
+	} else {
+		data = JSON.parse(data);
+	}
+};
+
+let saveDataToLocalStorage = function () {
+	localStorage.setItem(LS_DATA, JSON.stringify(data));
+};
+
+let initializeMissingData = function () {
+	let arr = [ { name: "referee", initialValue: { name: "", password: "" } },
+				{ name: "competition", initialValue: "line" },
+				{ name: "arena", initialValue: "" },
+				{ name: "round", initialValue: "" },
+				{ name: "currentRun", initialValue: null }];
+	
+	for (let i=0; i<arr.length; i++) {
+		if (data[arr[i].name] === undefined) {
+			data[arr[i].name] = arr[i].initialValue;
+		}
+	}
+	
+	saveDataToLocalStorage();
+};
+
 let showInitialScreen = function () {
 	/* shows last screen, otherwise first screen */
 	let currentScreen = localStorage.getItem(LS_CURRENT_SCREEN);
@@ -153,6 +264,8 @@ let showInitialScreen = function () {
 };
 
 let showScreen = function (screenNumber) {
+	let initFunction = [null, null, initScreen2, null, null, null, null, null, null][screenNumber];
+	if (initFunction !== null) { initFunction(); }
 	document.getElementById("screen-" + screenNumber).style.display = "";
 };
 
@@ -166,6 +279,39 @@ let changeScreen = function (screenNumberFrom, screenNumberTo) {
 	localStorage.setItem(LS_CURRENT_SCREEN, screenNumberTo);
 	
 	window.location.hash = "#" + screenNumberTo; // "disables" go-back-button of browser
+};
+
+let initScreen2 = function () {
+	let txt;
+	
+	// name of referee
+	txt = data["referee"]["name"];
+	document.getElementById("s2-txt-referee-name").innerHTML = txt;
+	
+	// competition
+	txt = "error";
+	if (data["competition"] == "line") {
+		txt = "Rescue Line";
+	} else if (data["competition"] == "entry") {
+		txt = "Rescue Line Entry";
+	}
+	document.getElementById("s2-txt-competition").innerHTML = txt;
+	
+	// arena
+	if (data["arena"].startsWith("Arena ")) {
+		txt = data["arena"].substring(6);
+	} else {
+		txt = data["arena"];
+	}
+	document.getElementById("s2-txt-arena").innerHTML = txt;
+	
+	// round
+	if (data["round"].startsWith("Round ")) {
+		txt = data["round"].substring(6);
+	} else {
+		txt = data["round"];
+	}
+	document.getElementById("s2-txt-round").innerHTML = txt;
 };
 
 let addScoringElement = function (type) {
@@ -186,3 +332,22 @@ let removeScoringElement = function (type) {
 	elem.style.background = "#f00";
 	setTimeout(function () { elem.style.background = "black"; }, 150);
 };
+
+let initializeInputs = function () {
+	document.getElementById("referee-name").value = data["referee"]["name"];
+	document.getElementById("referee-password").value = data["referee"]["password"];
+	document.getElementById("competition").value = data["competition"];
+	
+	let arena = data["arena"]; // data["arena"] will be overwritten by initializing competition-input with onChangeInputCompetition()
+	let round = data["round"]; // ... same here ...
+	
+	onChangeInputCompetition();
+	
+	data["arena"] = arena;
+	data["round"] = round;
+	
+	document.getElementById("arena").value = data["arena"];
+	document.getElementById("round").value = data["round"];
+	
+	saveDataToLocalStorage();
+}
