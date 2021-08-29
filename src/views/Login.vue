b730cae5886c5087692e9458c647226bc40f5a79
Niko Lockenvitz <nl@nikolockenvitz.de>
2021-08-29 22:35:22 +0200

    add login check

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,12 +20,23 @@
         <button v-on:click="tryLogin">{{ tc("buttonLogin") }}</button>
       </div>
     </form>
+
+  <div class="login-status">
+    <div v-if="$store.state.settings.loginStatus === true">
+      <img src="../assets/icons/successful.svg" />
+    </div>
+    <div v-else-if="$store.state.settings.loginStatus === false">
+      <img src="../assets/icons/failed.svg" />
+    </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import CustomTextInput from "../components/inputs/CustomTextInput.vue";
+
+const CHECK_LOGIN_PATH = "/api/v1/login_required";
 
 export default defineComponent({
   name: "Login",
@@ -42,7 +53,10 @@ export default defineComponent({
       return this.$t("login." + key);
     },
     setSettingFunction(name: string): Function {
-      return (value: string) => this.$store.commit("setSetting", { name, value });
+      return (value: string) => {
+        this.$store.commit("setSetting", { name, value });
+        this.$store.commit("setLoginStatus", null);
+      };
     },
     tryLogin() {
       const { username, password } = this.$store.state.settings;
@@ -53,8 +67,34 @@ export default defineComponent({
         this.$nextTick(() => (this.focusPasswordInput = false));
         return;
       }
-      // TODO
+      fetch(`${this.$store.state.settings.submitHost}${CHECK_LOGIN_PATH}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Authorization: "Basic " + btoa(username + ":" + password),
+        },
+      })
+        .then(async (response) => {
+          let text = await response.text();
+          if (response.status === 200 && text === "ok") {
+            this.$store.commit("setLoginStatus", true);
+          } else {
+            throw text;
+          }
+        })
+        .catch((error) => {
+          console.log({ msg: "failed", response: error.toString() });
+          this.$store.commit("setLoginStatus", false);
+        });
     },
   },
 });
 </script>
+
+<style scoped>
+.login-status img {
+  max-width: 30vw;
+  max-height: 30vh;
+  margin: 2em 0;
+}
+</style>
