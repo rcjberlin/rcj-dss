229178949f7cab5b46ce54a6c54e46f6abc4b4f4
Niko Lockenvitz <nl@nikolockenvitz.de>
2022-01-08 21:16:10 +0100

    register service worker when deploying app

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,7 +2,7 @@
 
 import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === "production" && process.env.PWA_SW === "enabled") {
+if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log("App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB");
