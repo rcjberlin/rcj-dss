18a08b005d21ca86bf0d86d250281bb7ef6b7744
Niko Lockenvitz <nl@nikolockenvitz.de>
2022-01-30 01:54:27 +0100

    deploy

---
+++ precache-manifest.29e6df1e803f3cae3809129f9cc011d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed9d5f3dfbfe9d63021",
+    "url": "/rcj-dss/css/app.bbaa9816.css"
+  },
+  {
+    "revision": "cbfa839a068c1c17feedc8a4e6b47e9b",
+    "url": "/rcj-dss/img/pause.cbfa839a.svg"
+  },
+  {
+    "revision": "81ebc833e9932320ea66fca9738e5a59",
+    "url": "/rcj-dss/img/start.81ebc833.svg"
+  },
+  {
+    "revision": "e66051227c0a22bfecffe31e43121a8f",
+    "url": "/rcj-dss/index.html"
+  },
+  {
+    "revision": "4ed9d5f3dfbfe9d63021",
+    "url": "/rcj-dss/js/app-legacy.72566bcc.js"
+  },
+  {
+    "revision": "cbde60ffa0cc2eca644d",
+    "url": "/rcj-dss/js/chunk-vendors-legacy.a4398208.js"
+  },
+  {
+    "revision": "6b11bc45d1e83a3146551205c27f6329",
+    "url": "/rcj-dss/manifest.json"
+  }
+]);
---
+++ precache-manifest.b127a297e6423262b648b6f8ebd369c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a180e6a09132f2498ed",
+    "url": "/rcj-dss/css/app.bbaa9816.css"
+  },
+  {
+    "revision": "cbfa839a068c1c17feedc8a4e6b47e9b",
+    "url": "/rcj-dss/img/pause.cbfa839a.svg"
+  },
+  {
+    "revision": "81ebc833e9932320ea66fca9738e5a59",
+    "url": "/rcj-dss/img/start.81ebc833.svg"
+  },
+  {
+    "revision": "face8848138e20d8c48e173ea5915aa3",
+    "url": "/rcj-dss/index.html"
+  },
+  {
+    "revision": "1a180e6a09132f2498ed",
+    "url": "/rcj-dss/js/app.ec0dd72b.js"
+  },
+  {
+    "revision": "a7e59c82792d14cb7889",
+    "url": "/rcj-dss/js/chunk-vendors.8c74b7a5.js"
+  },
+  {
+    "revision": "6b11bc45d1e83a3146551205c27f6329",
+    "url": "/rcj-dss/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rcj-dss/robots.txt"
+  }
+]);
